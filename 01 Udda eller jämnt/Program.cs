ba1a9b5057f64c3743d085127f3023131c86e873
Claes Engelin <claes.engelin@zocom.se>
2025-09-17 09:15:22 +0200

    Lagt till kod för övning 1

--- 01 Udda eller jämnt/Program.cs
+++ 01 Udda eller jämnt/Program.cs
@@ -2,3 +2,8 @@
  *  1. Skriv ett uttryck som kollar om ett givet heltal är udda eller jämnt
 */
 
+Console.Write("Ange ett heltal: ");
+int number = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Talet {0} är {1}", number, number % 2 == 0 ? "jämnt" : "udda");
+Console.WriteLine($"Talet {number} är {(number % 2 == 0 ? "jämnt" : "udda")}" );
