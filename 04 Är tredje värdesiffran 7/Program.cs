86136e659100618a5199a04c09d5f1584fa7b879
Claes Engelin <claes.engelin@zocom.se>
2025-09-17 11:41:21 +0200

    Lagt till kod för uppgift 8

--- 04 Är tredje värdesiffran 7/Program.cs
+++ 04 Är tredje värdesiffran 7/Program.cs
@@ -4,3 +4,7 @@
  *      Till exempel 1732 -> true.
 */
 
+Console.Write("Ange ett heltal (större än 99): ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Tredje siffran i {number} är 7: {number / 100 % 10 == 7}");
--- 07 Beräkna parallelltrapetsens area/Program.cs
+++ 07 Beräkna parallelltrapetsens area/Program.cs
@@ -2,3 +2,13 @@
  * 7.   Skriv ett uttryck som beräknar arean på en parallelltrapets givet 
  *      sidorna a och b samt höjden h.
 */
+
+Console.WriteLine("Parallelltrapetsens sidor:");
+Console.Write("     a = ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("     b = ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("     höjd = ");
+double h = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Parallelltraptsens area är {(a+b)/2 * h}");
